acddc0e4dd854d341e7418139ab8d336ea62afda
Sivanesh9791 <msivanesh55@gmail.com>
2026-04-18 08:46:03 +0530

    update axios baseURL for production

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
+  baseURL: 'https://somazon-backend-767v.onrender.com/api'
+})
 
-export default instance;
+export default instance
